253a505938f90f65671742c5668f5b252dd71c3f
shabaj <shabajansari843@gmail.com>
2021-10-18 17:31:53 +0530

    Added New Solution

--- Arrays/GratestNumbersOfCandies.java
+++ Arrays/GratestNumbersOfCandies.java
@@ -2,16 +2,45 @@
 
 package Arrays;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GratestNumbersOfCandies {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
-        int n = sc.nextInt();
 
-        int arr[][] = new int[m][n];
+        int arr[] = new int[m];
 
-        int 
+        int Extra = sc.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(kidsWithCandies(arr, Extra));
+
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        ArrayList<Boolean> arr = new ArrayList<>(candies.length);
+        int max = 0;
+        
+        
+        for(int i=0;i<candies.length;i++)
+            
+                max  = Math.max(max,candies[i]);
+
+         for(int i=0;i<candies.length;i++)
+         {
+            if(max<=(candies[i]+extraCandies))
+                 arr.add(true);
+            else
+                arr.add(false);
+         }
+        return arr;
+    
+
     }
 }
