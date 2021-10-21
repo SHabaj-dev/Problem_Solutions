1870b4fa718946b2900c0ffeb5ece0daedb03202
shabaj <shabajansari843@gmail.com>
2021-10-21 21:49:30 +0530

    21/10/2021

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,43 @@
+// Program to perform a Binary Search.
+package Arrays;
+
+import java.util.*;
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        int ans = binarySearch(arr, target);
+
+        System.out.println(ans);
+
+    }
+
+    public static int binarySearch(int[] a, int target) {
+        int start = 0;
+        int end = (a.length - 1);
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < a[mid]) {
+                end = mid - 1;
+            } else if (target > a[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+}
